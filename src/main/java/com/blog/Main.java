fe7ae18dbee3d0dbcc634216623c29e950013d3b
DanAparicio22 <daparicio52@gmail.com>
2017-08-31 18:02:12 -0400

    Second commit

--- src/main/java/com/blog/Main.java
+++ src/main/java/com/blog/Main.java
@@ -27,5 +27,6 @@ public class Main {
 	public static void main(String[] args) {
 		//System.out.println(cantPalabras("         ASDASD ASDAS ASDAS ADASDAS         "));
 		SpringApplication.run(Main.class, args);
+		hola
 	}
 }
